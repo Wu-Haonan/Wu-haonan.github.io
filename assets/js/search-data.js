9ecab6031708ebc2ca5d81542b0a31bb7d38598f
Wu-Haonan <hvw5426@psu.edu>
2026-06-24 15:32:59 +0000

    Deploying to gh-pages from @ Wu-Haonan/Wu-haonan.github.io@aa39e3c21bb828249cb1017746038a0f15e12948 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
         },{id: "post-momonmer-types-suprachromosomal-families-sfs-and-hors-identification",
         
           title: "Momonmer Types, Suprachromosomal Families (SFs) and HORs Identification",
